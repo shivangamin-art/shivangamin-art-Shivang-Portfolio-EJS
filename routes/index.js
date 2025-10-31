cd58204bee597dd86a9c592ee5539925dff00d71
shivangamin-art <shivang.amin@ontariotechu.net>
2025-10-31 18:58:25 -0400

    modified:   app.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Home */
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Home | Shivang Portfolio' });
+});
+
+/* About */
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About | Shivang Portfolio' });
+});
+
+/* Projects */
+router.get('/projects', (req, res) => {
+  res.render('projects', { title: 'Projects | Shivang Portfolio' });
+});
+
+/* Contact (GET) */
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact | Shivang Portfolio', submitted: false });
+});
+
+/* Contact (POST) */
+router.post('/contact', (req, res) => {
+  const { name, email, message } = req.body || {};
+  res.render('contact', {
+    title: 'Contact | Shivang Portfolio',
+    submitted: true,
+    name, email, message
+  });
 });
 
 module.exports = router;
